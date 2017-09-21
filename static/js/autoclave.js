10ad5b21d64fd3b11bd89c2a9a9b0a0d7f697935
hooper2chile <hooper.007@gmail.com>
2017-09-20 22:04:29 -0300

    cambios en autoclave web

--- static/js/autoclave.js
+++ static/js/autoclave.js
@@ -7,12 +7,15 @@ $(document).ready(function() {
 	    		       document.domain + ':' +
 		             location.port + namespace);
 
+
     //Se escuchan las mediciones de ph, OD, Temp.
     socket.on('Medidas', function(msg) {
         $('#med1').text('Ph: '    + msg.data[0]          ).html();
         $('#med2').text('OD: '    + msg.data[1] + ' %'   ).html();
         $('#med3').text('Temp: '  + msg.data[2] + ' º[C]').html();
     });
+
+
 
     //se emiten los setpoints hacia el servidor
     $('form#ac_setpoints').submit(function(event) {
@@ -31,7 +34,6 @@ $(document).ready(function() {
 	      console.log($('#temp_enable').is(':checked'));
         return false;
     });
-
     //se escuchan desde el servidor los setpoints aplicados
     //para ser desplegados en todos los clientes.
     socket.on('ac_setpoints', function(msg) {
@@ -39,13 +41,18 @@ $(document).ready(function() {
         document.getElementById('time').value   = msg.set[1];
         document.getElementById('temp_enable').checked = msg.set[2];
 	      document.getElementById('time_enable').checked = msg.set[3];
+        document.getElementById('time_set').value = msg.set[4];
+        document.getElementById('temp_set').value = msg.set[5];
         //para depurar
         console.log('Checkeds Recibidos');
         console.log($('#temp').val());
         console.log($('#time').val());
-	      console.log($('#time_enable').is(':checked')); 
+	      console.log($('#time_enable').is(':checked'));
 	      console.log($('#temp_enable').is(':checked'));
     });
+
+
+
 
     //se emiten señal de reinicio, apagado, grabacion y limpiaza hacia el servidor
     $('form#process').submit(function(event) {
