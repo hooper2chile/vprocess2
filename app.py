66673571206d8606aaf916797fcb9debbc05fed7
hooper2chile <hooper.007@gmail.com>
2017-09-22 00:05:00 -0300

    trabajando en autoclave app.py

--- app.py
+++ app.py
@@ -531,15 +531,16 @@ def background_thread2():
     global ac_sets, time_save, temp_save, thread2, measures, b
     flag_autoclave = True
 
-    while b >= temp_save and ac_sets[1] > 0:
-        socketio.sleep(1) # 60[s]
-        ac_sets[1] -= 1   # ac_sets[1]=: timer set
-        socketio.emit('ac_setpoints', {'set': ac_sets, 'save': [temp_save, time_save]}, namespace='/biocl', broadcast=True)
+    while flag_autoclave:
+        while b >= temp_save and ac_sets[1] > 0:
+            socketio.sleep(1) # 60[s]
+            ac_sets[1] -= 1   # ac_sets[1]=: timer set
+            socketio.emit('ac_setpoints', {'set': ac_sets, 'save': [temp_save, time_save]}, namespace='/biocl', broadcast=True)
 
-    #permite volver a correr el thread una vez terminado un timer
-    thread2 = None
-    socketio.sleep(0.5) #para no matar el procesador cuando no pasa nada..
-    #acá habria que implementar una función que haga la comunicación con uc2
+        #permite volver a correr el thread una vez terminado un timer
+        thread2 = None
+        socketio.sleep(0.5) #para no matar el procesador cuando no pasa nada..
+        #acá habria que implementar una función que haga la comunicación con uc2
 
 
 
