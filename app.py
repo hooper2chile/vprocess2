21fd646eeda7621868c301a91f907fa3599add6e
hooper2chile <hooper.007@gmail.com>
2017-09-22 19:04:23 -0300

    trabajando en autoclave app.py

--- app.py
+++ app.py
@@ -531,7 +531,7 @@ def background_thread2():
     flag_autoclave = True
 
     while flag_autoclave:
-        while float(measures[2]) >= temp_save and ac_sets[1] > 0:
+        while float(measures[2]) >= temp_save and ac_sets[1]-1 >= 0:
             socketio.sleep(1) # 60[s]
             ac_sets[1] -= 1   # ac_sets[1]=: timer set
             socketio.emit('ac_setpoints', {'set': ac_sets, 'save': [temp_save, time_save]}, namespace='/biocl', broadcast=True)
