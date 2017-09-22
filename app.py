fe8681ea7667d50aa7395234a1403a6d6e9e5aaa
hooper2chile <hooper.007@gmail.com>
2017-09-22 18:19:10 -0300

    trabajando en autoclave app.py

--- app.py
+++ app.py
@@ -533,9 +533,10 @@ def background_thread2():
     while flag_autoclave:
         while float(measures[2]) >= temp_save and ac_sets[1] > 0:
             socketio.sleep(1) # 60[s]
-            ac_sets[1] = ac_sets[1] - 1   # ac_sets[1]=: timer set
+            ac_sets[1] -= 1   # ac_sets[1]=: timer set
             socketio.emit('ac_setpoints', {'set': ac_sets, 'save': [temp_save, time_save]}, namespace='/biocl', broadcast=True)
 
+        ac_sets[1] = 0
         #permite volver a correr el thread una vez terminado un timer
         thread2 = None
         socketio.sleep(0.5) #para no matar el procesador cuando no pasa nada..
