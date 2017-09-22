051b03549325d6a2f06b802d7ec2b426fc8399b7
hooper2chile <hooper.007@gmail.com>
2017-09-21 23:21:11 -0300

    trabajando en autoclave app.py

--- app.py
+++ app.py
@@ -545,7 +545,7 @@ def background_thread2():
 
     f = open(DIR + "debugging2.txt","a+")
     f.write("no entro al while\n...")
-    f.write('b='str(b) + ' ' + 'float(temp_save)=' + str(temp_save) + '\n' )
+    f.write('b='+ str(b) + ' ' + 'float(temp_save)=' + str(temp_save) + '\n' )
     f.close()
     #permite volver a correr el thread una vez terminado un timer
     thread2 = None
