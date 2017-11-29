edca5fb7208c89d66b0ef361cc6d443f2305f238
hooper2chile <hooper.007@gmail.com>
2017-11-29 13:24:23 -0300

    mejorando autoclave

--- firmware/uc_master/lib/mlibrary/mlibrary.h
+++ firmware/uc_master/lib/mlibrary/mlibrary.h
@@ -440,13 +440,13 @@ void control_ph() {
 //de la magnitud de esa temperatura que electro valvulas utiliza, si de agua o de vapor.
 //rst5 = : flag for heat heat_exchanger_controller
 void heat_exchanger_controller(char option) {
-  if ( rst5 == '1' ) { //and flag de autoclave (falta agregarlo)
+  if ( rst5 == 1 ) { //and flag de autoclave (falta agregarlo)
     if      ( option == 'v' ) signal = 'v';  //opcion 'v' (a-uto clave): destinado a operar los relay para vapor ('v')
     else if ( option == 'd' ) signal = 'd';  //opcion 'd' (d-efault): modo todo apagado
   }
 
   //opcion 'p' (p-roceso): se switchea las electrovalvulas para controlar temperatura con agua caliente ('a') o vapor ('v')
-  if ( option == 'p' and rst5 == '0') {
+  if ( option == 'p' and rst5 == 0 ) {
       if (Temp1 < mytempset ) signal = 'v';
       if (Temp1 > mytempset ) signal = 'a';
   }
@@ -455,7 +455,7 @@ void heat_exchanger_controller(char option) {
 
 
 void motor_set() {  //opcion "m" motor: destinado a operar las rpm del motor
-  if (rst2 == '0') {
+  if ( rst2 == 0 ) {
     String command = 'm' + String(mymix);
     //"motor seteado"
   }
